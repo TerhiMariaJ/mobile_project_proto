cf5109e87b56380126095a08d488bd59cd5c4f5e
Terhi Järvinen <97961165+TerhiMariaJ@users.noreply.github.com>
2023-05-23 17:05:38 +0300

    UI: Start addUser and search
    Adduser working in ui, not in backend yet. Search has it's spot in xml but no functionality

--- Mobileproto/app/src/main/java/fi/tuni/mobile_proto/MainActivity.kt
+++ Mobileproto/app/src/main/java/fi/tuni/mobile_proto/MainActivity.kt
@@ -1,8 +1,9 @@
 package fi.tuni.mobile_proto
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.view.LayoutInflater
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.BufferedReader
@@ -12,15 +13,22 @@ import java.net.URL
 import kotlin.concurrent.thread
 
 
+
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var adapter: ArrayAdapter<Users>
+    private lateinit var alertDialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var lv: ListView = findViewById<ListView>(R.id.listView)
+        val addButton = findViewById<Button>(R.id.addButton)
+
+        addButton.setOnClickListener {
+            addUserPopUp()
+        }
 
         adapter = ArrayAdapter<Users>(this, R.layout.item, R.id.myTextView, ArrayList())
         lv.adapter = adapter
@@ -55,12 +63,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getUrl(url: String) : String? {
+        print("getUrl method called")
         var result : String? = null
         val sb = StringBuffer()
         val myUrl = URL(url)
         val conn = myUrl.openConnection() as HttpURLConnection
         val inputStream = conn.getInputStream()
-        val reader = BufferedReader(InputStreamReader(inputStream));
+        val reader = BufferedReader(InputStreamReader(inputStream))
 
         reader.use {
             var line : String ? = null
@@ -77,4 +86,47 @@ class MainActivity : AppCompatActivity() {
 
         return result
     }
+
+    fun addUserPopUp(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Add User")
+        val addView = LayoutInflater.from(this).inflate(R.layout.add_user, null)
+        builder.setView(addView)
+
+        val firstNameEdit = addView.findViewById<EditText>(R.id.firstNameEdit)
+        val lastNameEdit = addView.findViewById<EditText>(R.id.lastNameEdit)
+        val addNew = addView.findViewById<Button>(R.id.addNew)
+        val cancelButton = addView.findViewById<Button>(R.id.cancelButton)
+
+
+        addNew.setOnClickListener {
+            val firstName = firstNameEdit.text.toString()
+            val lastName = lastNameEdit.text.toString()
+
+
+            if (firstName.isNotEmpty() && lastName.isNotEmpty()) {
+                val newUser = Users(firstName, lastName)
+                addUser(newUser)
+                alertDialog.dismiss()
+            } else {
+                Toast.makeText(this, "Please enter valid names", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        cancelButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    fun addUser(user: Users) {
+        runOnUiThread {
+            adapter.add(user)
+        }
+    }
+
+
+
 }
--- Mobileproto/app/src/main/java/fi/tuni/mobile_proto/Users.kt
+++ Mobileproto/app/src/main/java/fi/tuni/mobile_proto/Users.kt
@@ -3,4 +3,4 @@ package fi.tuni.mobile_proto
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Users(var name: String? = null, var mass: Int = 0, var height: Int = 0)
+data class Users(var firstName: String? = null, var lastName: String? = null)
